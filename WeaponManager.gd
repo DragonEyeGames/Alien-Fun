a363ed391620d41624abd13cf615c100b55a5fc9
Taxalottle Syndicate <bthole41@gmail.com>
2025-11-29 12:12:25 -0500

    Level up text
    there is now level up text thingy that shows up when you go to level up your bro!

--- WeaponManager.gd
+++ WeaponManager.gd
@@ -7,7 +7,19 @@ var items = {
 			"damage": 5,
 			"speed": 2,
 			"size": 1.0,
-			"count": 1,
+			"count": 1
+		},
+		"upgrades" : {
+			"damage": 1.2,
+			"speed": 1.5,
+			"size": 1.1,
+			"count": 1
+		},
+		"upgrade_type": {
+		"damage": "mul",
+		"speed": "mul",
+		"size": "mul",
+		"count": "add"
 		}
 	},
 	"fist": {
@@ -16,13 +28,31 @@ var items = {
 			"damage": 1,
 			"size": .5,
 			"count": 5,
+		},
+		"upgrades" : {
+			"damage": 1.2,
+			"size": 1.1,
+			"count": 2
+		},
+		"upgrade_type": {
+		"damage": "mul",
+		"size": "mul",
+		"count": "add"
 		}
 	},
 	"player": {
 		"level": 1,
 		"stats" : {
 			"health": 100,
-			"speed": 1,
+			"speed": 1
+		},
+		"upgrades" : {
+			"health": 1.05,
+			"speed": 1.05
+		},
+		"upgrade_type": {
+			"health": "mul",
+			"speed": "mul"
 		}
 	},
 }
--- hud.gd
+++ hud.gd
@@ -3,8 +3,10 @@ extends CanvasLayer
 @export var player: Player
 var xpOffset:=0.0
 var bar: ProgressBar
+
 func _ready() -> void:
 	bar=$ProgressBar
+	levelUp()
 	
 func _process(_delta: float) -> void:
 	bar.value=player.xp-xpOffset
@@ -16,8 +18,42 @@ func _process(_delta: float) -> void:
 		
 func levelUp():
 	get_tree().paused=true
+	var upgradeChoices = WeaponManager.items.duplicate()
+	var keys = upgradeChoices.keys()
+	for child in $LevelUp/VBoxContainer.get_children():
+		var random_key = keys.pick_random()
+		var random_item = upgradeChoices[random_key]
+		keys.erase(random_key)
+		update(child, random_item, random_key)
 	$LevelUp.visible=true
 
+func update(node, item, keyName):
+	var upgradeChoices = item["stats"].duplicate()
+	var keys=upgradeChoices.keys()
+	
+	var upgrade1 = keys.pick_random()
+	print(upgrade1)
+	keys.erase(upgrade1)
+	var upgrade2 = keys.pick_random()
+	print(upgrade2)
+	keys.erase(upgrade2)
+	
+	node.get_node("title").text=str(keyName)
+	
+	var current1=WeaponManager.items[keyName]["stats"][upgrade1]
+	var final1
+	if(WeaponManager.items[keyName]["upgrade_type"][upgrade1]=="mul"):
+		final1=current1*WeaponManager.items[keyName]["upgrades"][upgrade1]
+	else:
+		final1=current1+WeaponManager.items[keyName]["upgrades"][upgrade1]
+		
+	var current2=WeaponManager.items[keyName]["stats"][upgrade2]
+	var final2
+	if(WeaponManager.items[keyName]["upgrade_type"][upgrade2]=="mul"):
+		final2=current2*WeaponManager.items[keyName]["upgrades"][upgrade2]
+	else:
+		final2=current2+WeaponManager.items[keyName]["upgrades"][upgrade2]
+	node.get_node("description").text=str(upgrade1) + ": " + str(current1) + " -> " + str(final1) +  "    " + str(upgrade2) + ": " + str(current2) + " -> " + str(final2)
 
 func _on__pressed() -> void:
 	print("1")
